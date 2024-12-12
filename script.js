037891145aab7f48a51242ca8786caf25f33c5f5
Asif-2006 <asif21012006@gmail.com>
2024-12-13 02:42:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const box = document.querySelector("#box");
+const container = document.querySelector("#container");
+const button = document.querySelector("button");
+
+
+button.addEventListener("click",()=>{
+   if(box.value==='') alert("You must write something");
+   else{
+    let li = document.createElement("li");
+    li.innerHTML = box.value;
+    container.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    // container.style.justifyContent = "space-between";
+    // span.style.marginLeft = "284px";
+   }
+   box.value = '';
+})
+
+container.addEventListener("click",(e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+})
